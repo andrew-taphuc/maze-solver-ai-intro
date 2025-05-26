563d68acb33603388fa6acb2c18adcf3da0a4dcf
Nguyen Manh Tung <nmtung.work@gmail.com>
2025-05-26 19:19:42 +0700

    commit

---
+++ src/search/bidirectionalbfs.py
@@ -0,0 +1,13 @@
+def solve_maze_bidirectional_bfs(maze, start, end):
+    """
+    Giải mê cung sử dụng thuật toán Bidirectional BFS
+    
+    Args:
+        maze: Ma trận mê cung
+        start: Vị trí bắt đầu (x, y)
+        end: Vị trí kết thúc (x, y)
+    
+    Returns:
+        List các vị trí tạo thành đường đi từ start đến end
+    """
+    pass 
