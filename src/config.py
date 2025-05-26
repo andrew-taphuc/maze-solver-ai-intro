5fb1f4d5c4a63dd03288a8eaf54b50ef2fc15e72
Ta Hong Phuc <taphuc1@gmail.com>
2025-05-26 16:32:15 +0700

    define CELL class

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
+# Game Resolution
+RESOLUTION = WIDTH, HEIGHT = 1203, 724
+# Cells' size (pixel)
+TILE_SIZE = 40
+# Number of rows, columns
+cols, rows = 24, 18
+# Starting position of the maze (top left)
+MAZE_OFFSET = 240
 BACKGROUND_COLOR = "#1e1e1e"
 WALL_COLOR = "#f70067"
 BUTTON_COLOR = "#0d802b"
